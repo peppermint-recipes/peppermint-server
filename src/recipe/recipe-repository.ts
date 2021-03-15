cc6a283835daeb27ad63fe43f9d812c2b5091e9b
André Sterba <git@sterba.dev>
2021-03-15 10:08:37 +0100

    Remove old code

--- src/recipe/recipe-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { EntityRepository, Repository } from "typeorm";
-
-import { Recipe } from "./recipe-model";
-
-@Injectable()
-@EntityRepository(Recipe)
-export class RecipeRepository extends Repository<Recipe> {}
