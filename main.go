37677ebe385fc7d514945170193652f35345c827
André Sterba <git@sterba.dev>
2021-03-27 14:21:21 +0100

    Quick fix

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/peppermint-recipes/peppermint-server/config"
 	"github.com/peppermint-recipes/peppermint-server/database"
@@ -15,10 +17,37 @@ func livezHandler(c *gin.Context) {
 	})
 }
 
+// TODO: CORS Middleware
+// Copied from https://asanchez.dev/blog/cors-golang-options/
+func CORS(c *gin.Context) {
+
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+
+		c.Next()
+
+	} else {
+
+		// Everytime we receive an OPTIONS request,
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
+
 func setupServer(dbConfig *config.DBConfig) *gin.Engine {
 	database.RegisterConnection(dbConfig.Username, dbConfig.Password, dbConfig.Endpoint)
 
 	router := gin.Default()
+	router.Use(CORS)
 	recipeServer := recipe.NewRecipeServer()
 	weekplanServer := weekplan.NewWeekplanServer()
 	shoppingListServer := shoppinglist.NewShoppingListServer()
