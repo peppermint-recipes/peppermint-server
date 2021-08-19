cb7e47588da1b160e404bc8c3d147a9b09ab1b42
André Sterba <git@sterba.dev>
2021-08-19 09:35:58 +0200

    Add basic tests

--- main_test.go
+++ main_test.go
@@ -34,3 +34,81 @@ func TestLivezRoute(t *testing.T) {
 		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
 	}
 }
+
+func TestRecipeRoute(t *testing.T) {
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/recipes", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %v", response.StatusCode)
+	}
+
+	val, ok := response.Header["Content-Type"]
+
+	if !ok {
+		t.Fatalf("Expected Content-Type header to be set")
+	}
+
+	if val[0] != "application/json; charset=utf-8" {
+		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
+}
+
+func TestWeekplanRoute(t *testing.T) {
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/weekplans", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %v", response.StatusCode)
+	}
+
+	val, ok := response.Header["Content-Type"]
+
+	if !ok {
+		t.Fatalf("Expected Content-Type header to be set")
+	}
+
+	if val[0] != "application/json; charset=utf-8" {
+		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
+}
+
+func TestShoppingListRoute(t *testing.T) {
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/shopping-lists", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %v", response.StatusCode)
+	}
+
+	val, ok := response.Header["Content-Type"]
+
+	if !ok {
+		t.Fatalf("Expected Content-Type header to be set")
+	}
+
+	if val[0] != "application/json; charset=utf-8" {
+		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
+}
