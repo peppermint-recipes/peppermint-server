6d5d64e2c991f2fa741de9f6e407590c38fba7fa
André Sterba <48120735+andresterba@users.noreply.github.com>
2021-08-20 19:56:33 +0000

    Add basic e2e tests (#212)

---
+++ config/config_test.go
@@ -0,0 +1,116 @@
+package config
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+var testingEnvVaribles = map[string]string{
+	"DATABASE_ENDPOINT": "127.0.0.1:1337",
+	"DATABASE_USER":     "my-user",
+	"DATABASE_PASSWORD": "example-pw",
+	"WEBSERVER_ADDRESS": "10.13.37.1",
+	"WEBSERVER_PORT":    "1338",
+}
+
+func setTestEnvVariables() {
+	for key, value := range testingEnvVaribles {
+		os.Setenv(key, value)
+	}
+}
+
+func unsetTestEnvVariables() {
+	for key := range testingEnvVaribles {
+		os.Unsetenv(key)
+	}
+}
+
+func TestGetFromEnvAsString(t *testing.T) {
+	type args struct {
+		key          string
+		defaultValue string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"TestDefault", args{"wasd", "default"}, "default"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := (tt.args.defaultValue); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getFromEnvAsString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetFromEnvAsInt(t *testing.T) {
+	type args struct {
+		key          string
+		defaultValue int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"TestDefault", args{"wasd", 1}, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := (tt.args.defaultValue); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getFromEnvAsString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetConfigWithDefaultValues(t *testing.T) {
+	unsetTestEnvVariables()
+	config := GetConfig()
+
+	if config.DB.Endpoint != "127.0.0.1:27017" {
+		t.Errorf("config.DB.Endpoint = %s; want 127.0.0.1:27017", config.DB.Endpoint)
+	}
+	if config.DB.Username != "root" {
+		t.Errorf("config.DB.Username = %s; want root", config.DB.Username)
+	}
+	if config.DB.Password != "example" {
+		t.Errorf("config.DB.Password = %s; want example", config.DB.Password)
+	}
+	if config.Web.Address != "127.0.0.1" {
+		t.Errorf("config.Web.Address = %s; want 127.0.0.1", config.Web.Address)
+	}
+	if config.Web.Port != "1337" {
+		t.Errorf("config.Web.Port = %s; want 1337", config.Web.Port)
+	}
+}
+
+func TestGetConfigWithValuesFromEnvironment(t *testing.T) {
+	setTestEnvVariables()
+	config := GetConfig()
+
+	if config.DB.Endpoint != "127.0.0.1:1337" {
+		t.Errorf("config.DB.Endpoint = %s; want 127.0.0.1:1337", config.DB.Endpoint)
+	}
+	if config.DB.Username != "my-user" {
+		t.Errorf("config.DB.Username = %s; want my-user", config.DB.Username)
+	}
+	if config.DB.Password != "example-pw" {
+		t.Errorf("config.DB.Password = %s; want example-pw", config.DB.Password)
+	}
+	if config.Web.Address != "10.13.37.1" {
+		t.Errorf("config.Web.Address = %s; want 10.13.37.1", config.Web.Address)
+	}
+	if config.Web.Port != "1338" {
+		t.Errorf("config.Web.Port = %s; want 1338", config.Web.Port)
+	}
+	unsetTestEnvVariables()
+}
--- main_test.go
+++ main_test.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/peppermint-recipes/peppermint-server/config"
+	"github.com/peppermint-recipes/peppermint-server/database"
+	"github.com/peppermint-recipes/peppermint-server/recipe"
+	shoppinglist "github.com/peppermint-recipes/peppermint-server/shopping-list"
+	"github.com/peppermint-recipes/peppermint-server/weekplan"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestLivezRoute(t *testing.T) {
-	config := config.GetConfig()
-	testServer := httptest.NewServer(setupServer(config.DB))
-	defer testServer.Close()
+func beforeTestHook(t *testing.T) {
+	clearDatabase(t)
+}
 
-	response, err := http.Get(fmt.Sprintf("%s/livez", testServer.URL))
+func afterTestHook(t *testing.T) {
+	clearDatabase(t)
+}
 
+func clearDatabase(t *testing.T) {
+	client, ctx, _ := database.GetConnection()
+
+	err := client.Database(database.DatabaseName).Drop(ctx)
 	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
+		t.Fatalf("Could not drop database %v", err)
 	}
+}
 
+func validateHttp200Response(t *testing.T, response *http.Response) {
 	if response.StatusCode != 200 {
 		t.Fatalf("Expected status code 200, got %v", response.StatusCode)
 	}
@@ -32,5 +48,300 @@ func TestLivezRoute(t *testing.T) {
 
 	if val[0] != "application/json; charset=utf-8" {
 		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
+}
+
+func TestLivezRoute(t *testing.T) {
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/livez", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	validateHttp200Response(t, response)
+}
+
+func TestRecipeRouteGet(t *testing.T) {
+	beforeTestHook(t)
+	defer afterTestHook(t)
+
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/recipes", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	validateHttp200Response(t, response)
+}
+
+func TestRecipeRouteCreate(t *testing.T) {
+	beforeTestHook(t)
+	defer afterTestHook(t)
+
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	newRecipe := recipe.Recipe{
+		Name:         "my-recipe",
+		ActiveTime:   1,
+		TotalTime:    2,
+		Servings:     2,
+		Categories:   []string{"test"},
+		Ingredients:  "banana",
+		Instructions: "test all the things",
+		UserID:       "1",
+		Deleted:      false,
+		Calories:     1337,
+	}
+	json_data, err := json.Marshal(newRecipe)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createResponse, err := http.Post(fmt.Sprintf("%s/recipes", testServer.URL), "application/json; charset=utf-8", bytes.NewBuffer(json_data))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, createResponse)
+
+	getResponse, err := http.Get(fmt.Sprintf("%s/recipes", testServer.URL))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, getResponse)
+
+	var p []recipe.Recipe
+
+	err = json.NewDecoder(getResponse.Body).Decode(&p)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	newRecipe.ID = p[0].ID
+	newRecipe.LastUpdated = p[0].LastUpdated
+
+	if !cmp.Equal(p[0], newRecipe) {
+		t.Fatalf("Expected recipe %v to be equal %v", newRecipe, p[0])
+	}
+}
+
+func TestWeekplanRoute(t *testing.T) {
+	beforeTestHook(t)
+	defer afterTestHook(t)
+
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/weekplans", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	validateHttp200Response(t, response)
+}
+
+func TestWeekplanRouteCreate(t *testing.T) {
+	beforeTestHook(t)
+	defer afterTestHook(t)
+
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	testRecipe := recipe.Recipe{
+		Name:         "my-recipe",
+		ActiveTime:   1,
+		TotalTime:    2,
+		Servings:     2,
+		Categories:   []string{"test"},
+		Ingredients:  "banana",
+		Instructions: "test all the things",
+		UserID:       "1",
+		Deleted:      false,
+		Calories:     1337,
+	}
+	testRecipeAsJSON, err := json.Marshal(testRecipe)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createResponse, err := http.Post(
+		fmt.Sprintf("%s/recipes", testServer.URL),
+		"application/json; charset=utf-8",
+		bytes.NewBuffer(testRecipeAsJSON),
+	)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, createResponse)
+
+	getResponse, err := http.Get(fmt.Sprintf("%s/recipes", testServer.URL))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, getResponse)
+
+	var p []recipe.Recipe
+
+	err = json.NewDecoder(getResponse.Body).Decode(&p)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	testRecipe.ID = p[0].ID
+	testRecipe.LastUpdated = p[0].LastUpdated
+
+	testDay1 := weekplan.Day{
+		Breakfast: []recipe.Recipe{},
+		Lunch:     []recipe.Recipe{testRecipe},
+		Snack:     []recipe.Recipe{testRecipe},
+		Dinner:    []recipe.Recipe{},
+	}
+
+	testDay2 := weekplan.Day{
+		Breakfast: []recipe.Recipe{testRecipe},
+		Lunch:     []recipe.Recipe{},
+		Snack:     []recipe.Recipe{},
+		Dinner:    []recipe.Recipe{testRecipe},
+	}
+
+	newWeekplan := weekplan.WeekPlan{
+		UserID:       "1",
+		CalendarWeek: 1,
+		Year:         2021,
+		Monday:       testDay1,
+		Tuesday:      testDay2,
+		Wednesday:    testDay1,
+		Thursday:     testDay2,
+		Friday:       testDay1,
+		Saturday:     testDay2,
+		Sunday:       testDay1,
+	}
+
+	testWeekPlanAsJSON, err := json.Marshal(newWeekplan)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createResponseWeekPlan, err := http.Post(
+		fmt.Sprintf("%s/weekplans", testServer.URL),
+		"application/json; charset=utf-8",
+		bytes.NewBuffer(testWeekPlanAsJSON),
+	)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, createResponseWeekPlan)
+
+	getResponseWeekPlan, err := http.Get(fmt.Sprintf("%s/weekplans", testServer.URL))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, getResponseWeekPlan)
+
+	var w []weekplan.WeekPlan
+
+	err = json.NewDecoder(getResponseWeekPlan.Body).Decode(&w)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	newWeekplan.ID = w[0].ID
+	newWeekplan.LastUpdated = w[0].LastUpdated
+
+	if !cmp.Equal(w[0], newWeekplan) {
+		t.Fatalf("Expected weekplans %v to be equal %v", newWeekplan, w[0])
+	}
+}
+
+func TestShoppingListRoute(t *testing.T) {
+	beforeTestHook(t)
+	defer afterTestHook(t)
+
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	response, err := http.Get(fmt.Sprintf("%s/shopping-lists", testServer.URL))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	validateHttp200Response(t, response)
+}
+
+func TestShoppingListRouteCreate(t *testing.T) {
+	beforeTestHook(t)
+	defer afterTestHook(t)
+
+	config := config.GetConfig()
+	testServer := httptest.NewServer(setupServer(config.DB))
+	defer testServer.Close()
+
+	testItem1 := shoppinglist.ShoppingListItem{
+		Ingredient: "Test1",
+		Unit:       "my-unit",
+		Amount:     100,
+	}
+
+	testItem2 := shoppinglist.ShoppingListItem{
+		Ingredient: "Test2",
+		Unit:       "my-unit-2",
+		Amount:     200,
+	}
+
+	newShoppingList := shoppinglist.ShoppingList{
+		UserID: "1",
+		Items:  []shoppinglist.ShoppingListItem{testItem1, testItem2},
+	}
+
+	testWeekPlanAsJSON, err := json.Marshal(newShoppingList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createResponseShoppingList, err := http.Post(
+		fmt.Sprintf("%s/shopping-lists", testServer.URL),
+		"application/json; charset=utf-8",
+		bytes.NewBuffer(testWeekPlanAsJSON),
+	)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, createResponseShoppingList)
+
+	getResponseShoppingList, err := http.Get(fmt.Sprintf("%s/shopping-lists", testServer.URL))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	validateHttp200Response(t, getResponseShoppingList)
+
+	var w []shoppinglist.ShoppingList
+
+	err = json.NewDecoder(getResponseShoppingList.Body).Decode(&w)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	newShoppingList.ID = w[0].ID
+	newShoppingList.LastUpdated = w[0].LastUpdated
+
+	if !cmp.Equal(w[0], newShoppingList) {
+		t.Fatalf("Expected shopingLists %v to be equal %v", newShoppingList, w[0])
 	}
 }
